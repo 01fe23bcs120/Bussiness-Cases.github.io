72f474cd9818b46f8db8d3581ed75b0b71b0a132
01fe23bcs120 <01fe23bcs120@kletech.ac.in>
2024-12-25 11:15:49 +0530

    Update Energy_Dist.cpp

--- Energy_Dist.cpp
+++ Energy_Dist.cpp
@@ -6,64 +6,60 @@ using namespace std;
 
 struct Edge {
     int u, v, weight;
-    bool operator<(const Edge &other) const {
-        return weight < other.weight;
-    }
 };
 
-class DisjointSet {
-public:
-    vector<int> parent, rank;
-    DisjointSet(int n) {
-        parent.resize(n);
-        rank.resize(n, 0);
-        for (int i = 0; i < n; ++i) parent[i] = i;
+bool compareEdges(const Edge &a, const Edge &b) {
+    return a.weight < b.weight; // Compare edges by weight
+}
+
+int find(int u, vector<int> &parent) {
+    if (parent[u] != u) {
+        parent[u] = find(parent[u], parent); // Path compression
     }
+    return parent[u];
+}
 
-    int find(int u) {
-        if (parent[u] != u) {
-            parent[u] = find(parent[u]); // Path compression
-        }
-        return parent[u];
+bool unionSets(int u, int v, vector<int> &parent, vector<int> &rank) {
+    int rootU = find(u, parent);
+    int rootV = find(v, parent);
+
+    if (rootU == rootV) return false; // Already in the same set
+
+    if (rank[rootU] > rank[rootV]) {
+        parent[rootV] = rootU;
+    } else if (rank[rootU] < rank[rootV]) {
+        parent[rootU] = rootV;
+    } else {
+        parent[rootV] = rootU;
+        rank[rootU]++;
     }
-
-    bool unionSets(int u, int v) {
-        int rootU = find(u);
-        int rootV = find(v);
-
-        if (rootU == rootV) return false;
-
-        if (rank[rootU] > rank[rootV]) {
-            parent[rootV] = rootU;
-        } else if (rank[rootU] < rank[rootV]) {
-            parent[rootU] = rootV;
-        } else {
-            parent[rootV] = rootU;
-            rank[rootU]++;
-        }
-        return true;
-    }
-};
+    return true;
+}
 
 int kruskalMST(int nodes, vector<Edge> &edges) {
-    sort(edges.begin(), edges.end());
-    DisjointSet ds(nodes);
+    sort(edges.begin(), edges.end(), compareEdges); // Sort edges by weight
+    vector<int> parent(nodes), rank(nodes, 0);
+
+    // Initialize disjoint sets
+    for (int i = 0; i < nodes; ++i) {
+        parent[i] = i;
+    }
 
     int mstCost = 0;
     for (const auto &edge : edges) {
-        if (ds.unionSets(edge.u, edge.v)) {
+        if (unionSets(edge.u, edge.v, parent, rank)) {
             mstCost += edge.weight;
             cout << "Edge included: " << edge.u << " - " << edge.v << " (Weight: " << edge.weight << ")\n";
         }
     }
+
     return mstCost;
 }
 
 int main() {
-    int nodes = 6; // Number of nodes in the network
-    int edgesCount = 9; // Number of connections
+    int nodes = 6;
     vector<Edge> edges = {
-        {0, 1, 4}, {0, 2, 4}, {1, 2, 2}, {1, 3, 5}, 
+        {0, 1, 4}, {0, 2, 4}, {1, 2, 2}, {1, 3, 5},
         {2, 3, 8}, {2, 4, 10}, {3, 4, 6}, {3, 5, 3}, {4, 5, 7}
     };
 
